53b529afb540eb705c716d904bbb38048465d0ca
hamidegumuslu <hamidegumuslu@gmail.com>
2017-08-10 14:24:29 +0300

    misafir kullanıcı post ekleyebiliyor,kaydedilen postlar listeleniyor

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-#site açıldığında nereye gitsin bilgisini root ile veriyoruz
-#rootta gösterilen: posts controllerın index actionına git
-#  root to: 'posts#index'
-
-#  get 'posts/:id', to: 'posts#show'
-#  post 'posts/:id', to: 'posts#create'
-  #  get 'about', to: "pages#about"
-  #  get 'index', to: "posts#index"
-  # post 'create', to: "posts#create"
-
-#post 'new', to: 'posts#create'
   root to: 'posts#index'
-  resource :posts
+  resources :posts
 end
