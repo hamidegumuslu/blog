6bcdef79f44be4d2196ef2bc83e46876b94a2a49
hamidegumuslu <hamidegumuslu@gmail.com>
2017-09-05 10:25:05 +0300

    resimler postta görüntüleniyor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body, category_ids: [])
+    params.require(:post).permit(:title, :image, :body, category_ids: [])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905064000) do
+ActiveRecord::Schema.define(version: 20170905064326) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
